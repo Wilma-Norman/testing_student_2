47a77394121278b9d3849d5d6c0d2cb6fa2f4f67
Wilma NOrman <wilmanorman999@gmail.com>
2024-10-18 11:05:03 +0200

    fixed styling

---
+++ src/components/NewMeassage/index.jsx
@@ -0,0 +1,35 @@
+'use client'
+import { useState } from "react";
+ 
+const NewMessage = ({ onSend }) => {
+    const [message, setMessage] = useState("");
+ 
+    const handleSend = () => {
+        if (message.trim()) {
+            onSend(message);
+            setMessage("");
+        }
+    };
+    
+    return (
+        <div className="bg-blue-200 my-10 p-10 rounded-xl w-fit mx-auto border-b-4 border-blue-300">
+            <div>
+                <input
+                    type="text"
+                    className="m-3 rounded-lg"
+                    placeholder="message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button
+                    className="w-16 p-3 m-3 bg-blue-300 rounded-lg"
+                    onClick={handleSend}>
+                    Send
+                </button>
+            </div>
+        </div>
+    );
+};
+ 
+export default NewMessage;
+ 
