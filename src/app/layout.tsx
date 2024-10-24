3c8e1ea0ae3e3263079a8d5e206154da6cdffca5
Wilma NOrman <wilmanorman999@gmail.com>
2024-10-24 13:19:16 +0200

    fixing list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import NewMessage from "@/components/NewMeassage";
 import List from "../components/List/index";
 import "./globals.css";
+import GuestList from "@/components/GuestList";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <List id={""} message={""} />
+        <GuestList listItem={null} />
         <NewMessage onSend={undefined}/>
         {children}
       </body>
