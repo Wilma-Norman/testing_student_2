cb7d4f0815191ff4015c00560a32a6f49df0c986
Wilma NOrman <wilmanorman999@gmail.com>
2024-10-31 10:45:33 +0100

    wrong brnach, switching to right

---
+++ src/components/GuestList/index.tsx
@@ -0,0 +1,31 @@
+import { ListType } from "@/utils/types";
+import { useState } from "react";
+import List from "../List";
+import NewMessage from "../NewMessage";
+import React from "react";
+ 
+type GuestListProps = {
+    listItem: ListType[] | null
+}
+ 
+const GuestList = ({ listItem }: GuestListProps) => {
+    const [messages, setMessages] = useState(listItem || []);
+ 
+    const addMessage = (message: string) => {
+        const newMessage = { id: messages.length + 1, message };
+        setMessages([...messages, newMessage]);
+    }
+ 
+    return (
+        <div className="bg-red-300">
+            <NewMessage onSend={addMessage} />
+            <div
+                className="bg-red-300"
+                data-testid="guest-list"
+            >
+                { messages && messages.map((item) => <List key={item.id} {...item} />)}
+            </div>
+        </div>
+    )
+}
+export default GuestList;
