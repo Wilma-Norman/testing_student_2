ce036c0ec68a053acf17d64959bdb4905a6a3685
Wilma NOrman <wilmanorman999@gmail.com>
2024-11-04 20:49:30 +0100

    fixed styling for List in mobile view

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -17,14 +17,14 @@ const List = ({ id, message }: ListType) => {
  
     return (
         <>
-            <div className={`flex gap-4 p-4 my-2 rounded-xl ${isImportant ? 'bg-yellow-200' : 'bg-pink-100'}`} key={id} data-testid="list-item">
-                <div className="md:w-full md:flex md:items-center md:text-xl text-sm">
+            <div className={`flex gap-4 p-4 my-2 rounded-xl  ${isImportant ? 'bg-yellow-200' : 'bg-pink-100'}`} key={id} data-testid="list-item">
+                <div className="md:w-full md:flex md:items-center md:text-xl text-sm w-full">
                     {isVisible ? message : ""}
                 </div>
-                <button className="md:bg-blue-200 md:w-24 md:rounded-xl md:p-2.5 bg-blue-200 w-24 rounded-xl text-sm" data-testid="toggle-message" onClick={toggleVisibility}>
+                <button className="md:bg-blue-200 md:w-24 md:rounded-xl md:p-2.5 bg-blue-200 w-24 rounded-xl text-sm p-2" data-testid="toggle-message" onClick={toggleVisibility}>
                     {isVisible ? "Hide" : "Show"}
                 </button>
-                <button className="md:bg-blue-200 md:w-24 md:rounded-xl md:p-2.5 bg-blue-200 w-24 rounded-xl text-sm" data-testid="toggle-important" onClick={toggleImportant}>
+                <button className="md:bg-blue-200 md:w-24 md:rounded-xl md:p-2.5 bg-blue-200 w-24 rounded-xl text-sm p-2" data-testid="toggle-important" onClick={toggleImportant}>
                     {isImportant ? "Unimportant" : "Important"}
                 </button>
             </div>
