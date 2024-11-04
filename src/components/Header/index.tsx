cc286f3ada2d6cc374a1ee3f12c7f5b3dc340f0b
Wilma NOrman <wilmanorman999@gmail.com>
2024-11-04 10:41:45 +0100

    fixed styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import SubTitle from "../SubTitle"
 
 const Head = () => {
     return(
-        <header className=''>
+        <header className='text-center m-0 p-8 bg-blue-200'>
             <h1 className='text-4xl font-bold text-pink-600 mb-2 tracking-wide'>Guest Book</h1>
             <SubTitle text="Leave a message down below..." />
         </header>
