9f6aa034d692d8eb28f2ee0fef5464ee0c171341
Wilma NOrman <wilmanorman999@gmail.com>
2024-11-04 12:01:15 +0100

    mergin everythinh correctly

--- src/components/SubTitle/index.tsx
+++ src/components/SubTitle/index.tsx
@@ -4,11 +4,7 @@ type SubtitleProps = {
  
 const SubTitle= ({text}:SubtitleProps) => {
     return (
-<<<<<<< HEAD
-        <h3 className='text-2xl'>{text}</h3>
-=======
         <h3 className='text-1xl text-pink-400 pt-4 font-light'>{text}</h3>
->>>>>>> subTitle1
     )
 }
 export default SubTitle;
